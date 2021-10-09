398f72828523da03d4f602410bf35998513b9541
Jason Elgin <jason@signllantern.com>
2021-10-09 10:57:01 -0400

    cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://signallantern.netlify.app",
     title: "signal lantern",
   },
   plugins: [
